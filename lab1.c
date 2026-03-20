b1a945efdf728d8de38889ba5c3a53547d13ab91
IglFox <iglfox@yandex.ru>
2026-03-20 23:32:09 +0300

    add methods of sort

--- lab1.c
+++ lab1.c
@@ -3,6 +3,23 @@
 #include <time.h>
 #include <math.h>
 
+
+
+// best = O(n) | worst and in common = O(n^2)
+void sortBubble(int* arr, int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+            }
+        }
+    }
+}
+
+
 void shakerSort(int arr[], int n) {
     int left = 0;
     int right = n - 1;
