a11a6f68e895fff494428c44f5f563567c905c53
IglFox <iglfox@yandex.ru>
2026-04-06 17:16:45 +0300

    test: action build test

--- makefile
+++ makefile
@@ -1,8 +1,7 @@
 .PHONY: venv clean
 
-lr1: src/labs/n1/lab1.c
+build_lr1: src/labs/n1/lab1.c
 	gcc src/labs/n1/lab1.c -o src/labs/n1/lab1.exe
-	./src/labs/n1/lab1.exe
 
 lr2: n2/lab2.c
 	gcc n2/lab2.c -o n2/lab2.exe -lm
--- src/labs/n1/lab1.c
+++ src/labs/n1/lab1.c
@@ -21,10 +21,7 @@ int main() {
     srand(time(NULL));
 
     int sizes[] = {
-        100, 200, 300, 400, 500,
-        600, 700, 800, 900, 1000,
-        1100, 1200, 1300, 1400, 1500,
-        1600, 1700, 1800, 1900, 2000
+        100, 200, 300, 400, 500
     };
     int numSizes = sizeof(sizes) / sizeof(sizes[0]);
     const int NUM_REPEATS = 10000;
