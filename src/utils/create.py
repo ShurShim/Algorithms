053e3da81c0666c2ec1e63231dc78a3231488354
Fertry <iglfox@yandex.ru>
2026-04-08 12:08:26 +0300

    Update create.py

--- src/utils/create.py
+++ src/utils/create.py
@@ -7,5 +7,7 @@ print(args)
 drawer = Drawer.Drawer(files_dir="src/data/")
 drawer.create_plot(x_label="n", y_label="t", output_file="src/data/graphs/graph1.png")
 drawer.create_plot_v2(
-    x_label="n", y_label="t", output_file="src/data/graphs/graph1.png"
+    x_label="Размер строки",
+    y_label="Время(мс)",
+    output_file="src/data/graphs/graph1.png",
 )
